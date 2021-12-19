6feff6159288804255a2f4a774b0474d85960873
Matheus Godoi <hidevelopmatt@gmail.com>
2021-12-19 18:25:55 -0300

    Add Schedule Card tests

---
+++ organisms/ScheduleCard/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import ScheduleCard from './index'
+
+const scheduleMock = {
+    title: 'Schedule Card',
+    status: 'pending',
+    due_on: '2022-01-13T00:00:00.000+05:30'
+}
+
+it('should render schedule title in Schedule Card', () => {
+    const { getByText } = render(<ScheduleCard todo={scheduleMock} />)
+    expect(getByText('Schedule Card')).toBeInTheDocument()
+})
